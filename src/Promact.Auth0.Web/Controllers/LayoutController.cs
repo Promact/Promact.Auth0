56edf1f088e1ed99b06a10b94fdf4dee97e2b4e0
Chintan Shah <chintanshah@promactinfo.com>
2022-09-03 14:37:47 +0530

    deletes old aspnetboilerplate project

--- src/Promact.Auth0.Web/Controllers/LayoutController.cs
+++
@@ -1,7 +0,0 @@
-namespace Promact.Auth0.Web.Controllers
-{
-    public class LayoutController : Auth0ControllerBase
-    {
-
-    }
-}
--- src/Promact.Auth0.Web/Startup/PageNames.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Promact.Auth0.Web.Startup
-{
-    public class PageNames
-    {
-        public const string Home = "Home";
-        public const string About = "About";
-    }
-}
